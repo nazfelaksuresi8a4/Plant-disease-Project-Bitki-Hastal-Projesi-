d6695a419e734ca1ac838440414469260204edd6
Aslan akbey <agmroc@gmail.com>
2026-01-24 22:31:02 +0300

    Delete Plant Disease Project directory

--- Plant Disease Project/artifical_intelligence.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import  Dense,Conv2D,MaxPooling2D
-from tensorflow.keras.preprocessing.image import  ImageDataGenerator
-from tensorflow.keras.preprocessing import  image
-from sklearn.preprocessing import  LabelEncoder
-from sklearn.model_selection import  train_test_split
-
