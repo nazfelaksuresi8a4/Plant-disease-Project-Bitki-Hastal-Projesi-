52dbdbab33e639ddca0a7b895d9b354188e4da59
Aslan akbey <agmroc@gmail.com>
2026-01-25 23:08:14 +0300

    Modify to_matrix to support multiple modes
    Added mode parameter to to_matrix method for handling different image processing modes.

--- PlantDiseaseProject/image_processing.py
+++ PlantDiseaseProject/image_processing.py
@@ -8,14 +8,27 @@ class İmageProcesser:
         self.status = True
 
     @staticmethod
-    def to_matrix(img):
-        try:
-            print(img)
-            if isinstance(img, str):
-                return cv.resize(cv.cvtColor(cv.imread(img), cv.COLOR_BGR2RGB), dsize=(224, 224))
-            else:
-                return cv.resize(cv.cvtColor(img, cv.COLOR_BGR2RGB), dsize=(224, 224))
+    def to_matrix(img,mode):
+        if mode == 'prediction':
+            try:
+                print(img)
+                if isinstance(img, str):
+                    return np.expand_dims(cv.resize(cv.cvtColor(cv.imread(img), cv.COLOR_BGR2RGB), dsize=(224, 224)),axis=0) / 255
+                else:
+                    return np.expand_dims(cv.resize(cv.cvtColor(img, cv.COLOR_BGR2RGB), dsize=(224, 224)),axis=0) / 255
 
-        except Exception as e0:
-            print(e0)
+            except Exception as e0:
+                print(e0)
+                return e0
+        
+        elif mode == 'matrix returner':
+            try:
+                print(img)
+                if isinstance(img, str):
+                    return cv.resize(cv.cvtColor(cv.imread(img), cv.COLOR_BGR2RGB), dsize=(224, 224)) / 255
+                else:
+                    return cv.resize(cv.cvtColor(img, cv.COLOR_BGR2RGB), dsize=(224, 224)) / 255
 
+            except Exception as e0:
+                print(e0)
+                return e0
