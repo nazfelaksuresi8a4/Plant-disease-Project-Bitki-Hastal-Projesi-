74d8feff82a5a5946ea291a00d397fc1e0e100c2
Aslan akbey <agmroc@gmail.com>
2026-01-29 04:02:02 +0300

    Update logPlotter.py

--- Project/logPlotter.py
+++ Project/logPlotter.py
@@ -1,31 +1,54 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-logfile_path = r"logs\datas.txt"
+logfile_path = r"logs\SigmoidModelLogs\datas.txt"
 
-dct = {}
-with open(logfile_path,mode='r') as logfile:
-    logs = logfile.read().split('\n')
+class Plotter:
+    def plotter(self,logfile_path):
+        dct = {}
+        dct_arr = []
+        with open(logfile_path,mode='r') as logfile:
+            logs = logfile.read().split('\n')
 
-    for log in logs:
-        log = log.split(':')
-        if len(log) == 2:
-            array = [float(x.strip()) for x in log[1].replace('[','').replace(']','').split(',')]
-            print(array)
-            dct[log[0]] = array
+            for log in logs:
+                log = log.split(':')
 
-nrow,ncol = 0,0
+                if len(log) == 2:
+                    array = [float(x.strip()) for x in log[1].replace('[','').replace(']','').split(',')]
+                    dct[log[0]] = array
+                    dct_arr.append(dct.copy())
+            
+                    dct.clear()
+                
+                else:
+                    print('xf')
 
-fig,ax = plt.subplots(nrows=3,ncols=3 - 1,figsize=(8,8))
-ax[len(ax) - 1,len(ax) - 2].axis(False)
+        for dc in dct_arr:
+            print(dc)
 
-for axX,key in enumerate(dct):
-    if ncol == len(ax) - 1:
-        nrow += 1
-        ncol = 0
+        nrow,ncol = 0,0
+        fig,ax = plt.subplots(nrows=3,ncols=3 - 1,figsize=(8,8))
 
-    ax[nrow,ncol].plot(np.array(dct[key]),label=str(key))
-    ax[nrow,ncol].legend()
-    ncol += 1 
+        for dct in dct_arr:
+            if nrow <= 1:
+                if ncol == len(ax) - 1:
+                    nrow += 1
+                    ncol = 0
 
-plt.show()
+                key,data = list(dct.items()).copy()[0]
+                print(nrow,ncol,key)
+
+                yield (nrow,ncol,dct,key)
+                ncol += 1
+            
+            else:
+                nrow,ncol = 0,0
+                key,data = list(dct.items()).copy()[0]
+                yield (nrow,ncol,dct,key)
+                
+                ncol += 1
+
+        else:
+            nrow,ncol = 0,0
+
+Plotter().plotter(logfile_path)
