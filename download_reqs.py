1854ac13892822aa97f25b3325c2729a2b03206b
Aslan akbey <agmroc@gmail.com>
2026-02-09 18:40:44 +0300

    Add files via upload

---
+++ download_reqs.py
@@ -0,0 +1,44 @@
+import subprocess as sbp
+import os as _o
+
+class installPythonPackages:
+    def __init__(self):
+        self.state = bool(0)
+        self.proces_out = None 
+    
+    def installPKGS(self):
+        try:
+            self.proces_out = sbp.Popen(['pip'])
+            self.state = 1
+
+        except Exception as e0fx:
+            self.state = 0
+            print(1,e0fx)
+        
+        finally:
+            try:
+                if self.state == 1:
+                    self.proces_out = sbp.Popen(['pip','install','-r','reqs.txt'],
+                                                               stdout=sbp.PIPE,
+                                                               stderr=sbp.STDOUT,
+                                                               text=True)
+                    
+                    if self.proces_out is not None:
+                        if isinstance(self.proces_out,list):
+                            self.state = 1
+                        
+                        else:
+                            print('proces in not list')
+                    
+                    else:
+                        print('process is none')
+                    
+                
+                else:
+                    print('python.exe -m pip command excepted falling down')
+            
+            except Exception as e0fx1:
+                print('Modüller kurulamıyor lütfen daha sonra tekrar deneyiniz hata: ' + str(e0fx1))
+                self.state = 0
+                self.proces_out = None
+            
